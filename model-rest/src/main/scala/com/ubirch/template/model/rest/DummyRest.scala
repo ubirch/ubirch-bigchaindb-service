a7e590a73d3fa8fbc4a8ffb1cb3bdf65d127ff09
Moellus <matthias.moeller@ubirch.com>
2017-09-26 09:59:11 +0200

    wip: Dockerfile*

--- model-rest/src/main/scala/com/ubirch/template/model/rest/DummyRest.scala
+++
@@ -1,7 +0,0 @@
-package com.ubirch.template.model.rest
-
-/**
-  * author: cvandrei
-  * since: 2017-06-23
-  */
-case class DummyRest(foo: String)
--- project/docker.sbt
+++
@@ -1,1 +0,0 @@
-addSbtPlugin("se.marcuslonnberg" % "sbt-docker" % "1.4.0")
